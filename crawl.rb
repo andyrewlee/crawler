2a7736df93a3cceba92c9e0c60e783ca626bedac
Andrew Lee <andyrewlee@gmail.com>
2015-12-03 23:22:47 -0800

    Abstract profile crawling to separate class

--- crawl.rb
+++ crawl.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require_relative 'crawler'
 
 # title
 # category
@@ -10,18 +11,5 @@ require 'nokogiri'
 
 url = "https://www.udemy.com/185774"
 
-begin
-  page = Nokogiri::HTML(open(url))
-  title = page.css('h1.course-title')[0].text.strip
-  social_media = page.css('.mt5 a').collect{ |node| node["href"] }
-  rate_count = page.css('.rate-count')[0].text.strip
-  instructor_name = page.css('.ins-name')[0].text
-  instructor_bio = page.css('.mt10 .js-simple-collapse-inner p').collect { |node| node.text.strip }
-  puts title
-  puts instructor_name
-  puts instructor_bio
-  puts rate_count
-  puts social_media
-rescue
-  puts "Error"
-end
+profile = ProfileCrawler.crawl(url)
+puts profile
---
+++ crawler.rb
@@ -0,0 +1,70 @@
+class ProfileCrawler
+  def self.crawl(url)
+    begin
+      page = Nokogiri::HTML(open(url))
+      title = page.css('h1.course-title')[0].text.strip
+      rate_count = page.css('.rate-count')[0].text.strip
+      instructor_name = page.css('.ins-name')[0].text
+      instructor_bio = page.css('.mt10 .js-simple-collapse-inner p').select { |node| !node.text.empty? }.collect { |node| node.text.strip }.join(" ")
+      social_media = page.css('.mt5 a').collect{ |node| node["href"] }
+
+
+      twitter = nil
+      facebook = nil
+      google = nil
+      youtube = nil
+      linkedin = nil
+      website = nil
+
+      social_media.each do |account|
+        if account.match("twitter")
+          twitter = account
+        elsif account.match("facebook")
+          facebook = account
+        elsif account.match("google")
+          google = account
+        elsif account.match("youtube")
+          youtube = account
+        elsif account.match("linkedin")
+          linkedin = account
+        else
+          website = account
+        end
+      end
+
+      profile = Profile.new(title, rate_count, instructor_name, instructor_bio, twitter, facebook, google, youtube, linkedin, website)
+      return profile
+    rescue
+      return nil
+    end
+  end
+end
+
+class Profile
+  attr_accessor :title, :rate_count, :instructor_name, :instructor_bio, :twitter, :facebook, :google, :youtube, :linkedin, :website
+  def initialize(title, rate_count, instructor_name, instructor_bio, twitter, facebook, google, youtube, linkedin, website)
+    @title = title
+    @rate_count = rate_count
+    @instructor_name = instructor_name
+    @instructor_bio = instructor_bio
+    @twitter = twitter
+    @facebook = facebook
+    @google = google
+    @youtube = youtube
+    @linkedin = linkedin
+    @website = website
+  end
+
+  def to_s
+    "Title: #{title}\n
+     Rate Count: #{rate_count}\n
+     Name: #{instructor_name}\n
+     Bio: #{instructor_bio}\n
+     Twitter: #{twitter}\n
+     Facebook: #{facebook}\n
+     Google: #{google}\n
+     Youtube: #{youtube}\n
+     Linkedin: #{linkedin}\n
+     Website: #{website}\n"
+  end
+end
